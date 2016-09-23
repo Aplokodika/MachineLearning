6d9424bbaf92f7de4527b3df456f52d20073e5c1
Sreram K <sreramk@outlook.com>
2016-09-23 22:41:29 +0530

    Add files via upload

--- src/mainFnc/MainFnc.java
+++ src/mainFnc/MainFnc.java
@@ -3,68 +3,80 @@
  */
 package mainFnc;
 
-import neuralNetwork.*;
-import neuralNetwork.activationFunctions.SinFunction;
+import neuralNetwork.activationFunctions.*;
+import neuralNetwork.graphicalAnalysis.ErrorGraph;
+import neuralNetwork.ioHandler.WeightIOHandler;
 import neuralNetwork.neuralInterface.NeuralNetworkInterface;
-import neuralNetwork.activationFunctions.LinearUnmodified;
+
+import java.awt.Color;
+import java.awt.Font;
 import java.util.*;
+
+import javax.swing.text.AttributeSet.ColorAttribute;
 
 public class MainFnc {
 
 	public static void main(String[] arg) throws Exception {
+
 		NeuralNetworkInterface neuralNetwork = new NeuralNetworkInterface();
+		WeightIOHandler weightIoHandle = new WeightIOHandler(neuralNetwork);
 
 		/// Depicts the neural network structure.
-		neuralNetwork.addSizeList(3); // the input neuron-count
-		neuralNetwork.addSizeList(4); // the hidden layer 1 with 3 neurons
-		// neuralNetwork.addSizeList(10);
-		// neuralNetwork.addSizeList(3);
+		neuralNetwork.addSizeList(1); // the input neuron-count
+		neuralNetwork.addSizeList(3);
+		neuralNetwork.addSizeList(3);
 
-		neuralNetwork.addSizeList(3, true); // output neuron
+		neuralNetwork.addSizeList(1, true); // output neuron
 
-		neuralNetwork.setActivationFunction(new SinFunction(), new LinearUnmodified());
-		neuralNetwork.setWeightValues(-1., 1.);
-		neuralNetwork.setBiasWeightValues(-.1, .1, 1.0);
+		neuralNetwork.setActivationFunction(new TanhFunction(), new LinearUnmodified());
+		neuralNetwork.setWeightValues(-1.0, 1.0);
+		neuralNetwork.setBiasWeightValues(-1., 1., 1.0);
 		neuralNetwork.setNetworkOutputComputationToQuick();
 		neuralNetwork.setWeightCap(-1.0, 1.0);
 
-		neuralNetwork.setLearningRateMomentum(0.5, 0.999);
-		Double errVal;
-		
-		neuralNetwork.setTrainingDataSet(new ArrayList<Double>(Arrays.asList(0.5, 0.4, 0.3)),
-				new ArrayList<Double>(Arrays.asList(0.9, 0.001, 0.002)));
-		neuralNetwork.setTrainingDataSet(new ArrayList<Double>(Arrays.asList(0.2, 0.2, 0.2)),
-				new ArrayList<Double>(Arrays.asList(0.9, 0.9, 0.9)));
-		neuralNetwork.setTrainingDataSet(new ArrayList<Double>(Arrays.asList(0.1, 0.1, 0.1)),
-				new ArrayList<Double>(Arrays.asList(0.9, 1.0, 0.8)), true);
+		neuralNetwork.setLearningRateMomentum(0.01, 0.1);
 
-		neuralNetwork.runTrainingSystem(false);
-		neuralNetwork.connect(new NeuronAddress(1, 1), new NeuronAddress(1,1), 1.0);
-		for (int ll = 0; ll < 1000; ll++) {
-			for (int i = 0; i < 1000; i++) {
-				errVal = neuralNetwork.runTrainingSystem( false);
-				System.out.println(ll + " Error = " + errVal);
-			}
-			neuralNetwork.setLoggedWeightValues();
+		for(int i = 1 ; i < 1000; i++){
+			double k = (1 - 2*Math.random());
+			neuralNetwork.setTrainingDataSet(new ArrayList<Double>(Arrays.asList(k)),
+					new ArrayList<Double>(Arrays.asList(Math.sin(k*2*Math.PI))), true);
 		}
-		//neuralNetwork.setLoggedWeightValues();
 
 		ArrayList<Double> ress;
-		double percentCal;
-		int count = 0;
+		weightIoHandle.setFileName("outdata.dat");
 
-		ress = neuralNetwork.networkResult(new ArrayList<Double>(Arrays.asList(0.5, 0.4, 0.3)));
-		for (int i = 0; i < ress.size(); i++) {
-			System.out.println("result = " + ress.get(i));
+		ErrorGraph graph = new ErrorGraph();
+		graph
+			.setFileName("graph.bmp")
+			.setImageFormat("bmp")
+			.setTrainingDataSet(neuralNetwork.dataSet)
+			.setNeuralNetwork(neuralNetwork.getNeuralTrainer().NNetwork);
+			
+		graph.setMinError(1.0e-4);
+		
+		for (int ll = 0; ll < 1; ll++) {
+			for (int i = 0; i < 1000; i++) {
+				neuralNetwork.runTrainingSystem(false);
+				graph.captureError();
+				System.out.println(" Cur Error = " + neuralNetwork.computeError(false));
+			}
+
+		}
+		for(int i = 0; i < 100; i++ ){
+			double k = (1 - 2*Math.random());
+			ress = neuralNetwork.networkResult(new ArrayList<Double>(Arrays.asList(k)));
+				System.out.println("->result = " + ress.get(0) + " Expected output = " + Math.sin(k*2*Math.PI));
 		}
 
-		ress = neuralNetwork.networkResult(new ArrayList<Double>(Arrays.asList(0.5, 0.4, 0.3)));
-		for (int i = 0; i < ress.size(); i++) {
-			System.out.println("2. result = " + ress.get(i));
-		}
 		System.out.println(" Cur Error = " + neuralNetwork.computeError(false));
-		percentCal = (double) count / 1000 * 100;
-		System.out.println("correct percentage :" + percentCal + "%");
-		System.out.println("Least recorded error = " + neuralNetwork.getLeastRecordedError());
+
+		
+		weightIoHandle.write();
+		graph
+		 	.setDuelColourForErrorIncrementAndDecrement(Color.GREEN, Color.YELLOW)
+		 	.createGraph(1000, 200, Color.GREEN,Color.RED, 20, 10, new Font("Times New Roman", Font.BOLD, 100), 
+		 					Color.GRAY, 10, 20)
+		 	.writeGraphToFile();
+		System.out.println("done");
 	}
 }
